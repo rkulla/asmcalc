05b20cca74d6ce41eee47ca4fd1258d211d40fc3
rkulla <rkulla@albatross.(none)>
2012-12-30 12:41:02 -0800

    Added more to the doctest

--- asmcalc.py
+++ asmcalc.py
@@ -44,10 +44,25 @@ def hex_chart(chart_len):
     Decimal to Hex chart function.
 
     >>> import asmcalc
-    >>> asmcalc.hex_chart(2)
+    >>> asmcalc.hex_chart(17)
     Decimal) Hex
     0) 0
     1) 1
+    2) 2
+    3) 3
+    4) 4
+    5) 5
+    6) 6
+    7) 7
+    8) 8
+    9) 9
+    10) A
+    11) B
+    12) C
+    13) D
+    14) E
+    15) F
+    16) 10
     """
     print "Decimal) Hex"
     for i in range(chart_len):
